c12e6b162e5d3033214e7dc4e5fc0bec51960a69
killianogorman <83577130+killianogorman@users.noreply.github.com>
2024-11-24 12:20:57 +0000

    Add UTM params to site

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,6 @@
 <nav class="flex flex-col sm:flex-row sm:justify-between w-full items-center">
     <div>
-        <a class="text-center" href="https://platform.payzone.ie/customer/11211/product-list/details/10284">GET YOUR TICKETS</a>
+        <a class="text-center" href="https://platform.payzone.ie/customer/11211/product-list/details/10284?utm_source=christmasconcert&utm_medium=website&utm_campaign=christmasconcert&ref=christmasconcert.github.io">GET YOUR TICKETS</a>
     </div>
     <div>
         <a class="text-center" href="/countdown">12.12.24</a>
